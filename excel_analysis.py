6be1fc5338973a488b136bab18a2e22850d4b921
Tian99 <jit29@pitt.edu>
2020-02-21 16:27:52 -0500

    Need to figure a way to read the txt file

--- excel_analysis.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def read(filename):
-	data = pd.read_excel(filename)
-	data.to_pickle('input/testing_set/testing_1/data.pkl')
-	print('Finish converting')
-
-read('input/testing_set/testing_1/file_1.xlsx')
