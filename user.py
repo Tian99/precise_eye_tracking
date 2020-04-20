5820e0aba7848ab3e33ca4c45e2c7b4f7bcf910d
Tian99 <jit29@pitt.edu>
2020-04-19 20:12:32 -0400

    Big changes, now adding human interaction

--- user.py
+++ user.py
@@ -1,20 +1,34 @@
 import sys
+import math
 from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5.QtGui import QIcon, QPixmap
 
 class MyWidget(QtWidgets.QWidget):
-    def __init__(self):
+    def __init__(self, parent):
         super().__init__()
+        self.setWindowTitle('User Input')
         self.setGeometry(30,30,600,400)
+        self.label = QtWidgets.QLabel(self)
+        self.pixmap = QPixmap('chosen_pic.png')
+        self.resize(self.pixmap.width(), self.pixmap.height())
+
         self.begin = QtCore.QPoint()
         self.end = QtCore.QPoint()
-        self.show()
 
     def paintEvent(self, event):
         qp = QtGui.QPainter(self)
-        br = QtGui.QBrush(QtGui.QColor(100, 10, 10, 40))  
+        qp.drawPixmap(self.rect(), self.pixmap)
+        br = QtGui.QBrush(QtGui.QColor(100, 10, 10, 90))  
         qp.setBrush(br)   
-        qp.drawEllipse(QtCore.QRect(self.begin, self.end))
-        print(self.begin, self.end)       
+        qp.drawRect(QtCore.QRect(self.begin, self.end))
+
+        begin_x = self.begin.x()
+        begin_y = self.begin.y()
+        end_x = self.end.x()
+        end_y = self.end.y()
+
+        radius = abs(math.sqrt((end_x - begin_x)**2 + (end_y - begin_y)**2))/2
+        print(radius)
 
     def mousePressEvent(self, event):
         self.begin = event.pos()
